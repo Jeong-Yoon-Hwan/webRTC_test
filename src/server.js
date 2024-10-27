54906dd88062b133d54139efa8dc4c4d61f2852f
jung-yoon-hwan <wjddbsghks5742@gmail.com>
2024-10-27 12:28:10 +0900

    project init

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log("Listening on http:js");
+
+app.listen(3000, handleListen);
